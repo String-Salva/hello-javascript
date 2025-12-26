3a41f12c4b89705a942a7ca8d8a82e1aedaa2ebc
Salva <string.salva@gmail.com>
2025-12-26 16:24:31 +0100

    Me he quedado en mitad de los operadores

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,20 +5,99 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 
 // 1. Escribe un comentario en una línea
 
+//Aquí está mi comentario
+
 // 2. Escribe un comentario en varias líneas
+
+/*Comentario
+de varias líneas
+brooo */
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+let nombre = "salva"
+let numero = 456.5
+let checkPrimo = false
+let antonioPalotes
+let vaper = null
+let numerogrande = 45645645645646456456456464545654446465645654645645645645645654654654654645645456456456456456546454656456456546565464564564564545646n
+let numerogrande2 = BigInt(54456456465465465654446465645654645645645645645654654654654645645456456544464656456546456456456456456546546546546456454564464645646)
+
+
+
+
 // 4. Imprime por consola el valor de todas las variables
+
+console.log(nombre)
+console.log(numero)
+console.log(checkPrimo)
+console.log(antonioPalotes)
+console.log(vaper)
+console.log(numerogrande)
+console.log(numerogrande2)
+
+
+
+
+
+
 
 // 5. Imprime por consola el tipo de todas las variables
 
+console.log(typeof(nombre))
+console.log(typeof(numero))
+console.log(typeof(checkPrimo))
+console.log(typeof(antonioPalotes))
+console.log(typeof(vaper))
+console.log(typeof(numerogrande))
+console.log(typeof(numerogrande2))
+
+
+
+
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+/*
+let nombre = "salvador"
+let numero = 7.5
+let checkPrimo = true
+let antonioPalotes
+let vaper = null
+let numerogrande = asd5654446465645654645645645645645654654654654645645456456456456456546454656456456546565464564564564545646n
+let numerogrande2 = BigInt(54456456465465465654446465645654645645645645645654654654654645645456456544464656456546456456456456456546546546546456454564464645646)
+
+*/
+
+
+
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
+/*
+let nombre = 5
+let numero = "salva"
+let checkPrimo = null
+let antonioPalotes = false
+let vaper = "verde"
+let numerogrande = 4
+let numerogrande2 = 5
+*/
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const name = "salvorio"
+const numeroPi = 3.14
+const checkPar = true
+//const antonioPalotero
+const vapero = null 
+const numGrande = BigInt(45646464654646546546464646456456)
 
 // 9. A continuación, modifica los valores de las constantes
+
+/*
+const name = "salvorio1"
+const numeroPi = 3.141
+const checkPar = false
+const antonioPalotero
+const vapero = null 
+const numGrande = BigInt(456s46464654646546546464646456456)
+*/
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
