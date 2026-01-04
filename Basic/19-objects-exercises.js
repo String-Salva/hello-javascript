dad2ef0a4d14ee7fe777a2dffb16a248b8230868
Salva <string.salva@gmail.com>
2026-01-04 02:13:45 +0100

    hasta desestructuracion y propagación

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -5,20 +5,85 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=15675
 
 // 1. Crea un objeto con 3 propiedades
 
+let esperancita = {
+  name: "Espe",
+  age: 30,
+  job: "farmacéutica",
+
+}
+
+
 // 2. Accede y muestra su valor
 
+console.log(esperancita.name)
+console.log(esperancita.age)
+console.log(esperancita.job)
+
+
 // 3. Agrega una nueva propiedad
+esperancita.pet = "Denver"
+console.log(esperancita.pet)
+console.log(esperancita)
 
 // 4. Elimina una de las 3 primeras propiedades
 
+delete esperancita.name
+console.log(esperancita)
+
 // 5. Agrega una función e invócala
+esperancita.work = function () {
+  console.log("Esperancita trabaja")
+}
+console.log(esperancita)
+esperancita.work()
+
 
 // 6. Itera las propiedades del objeto
 
+for (let propiedad in esperancita) {
+  console.log(`${propiedad} : ${esperancita[propiedad]}`)
+}
+
+console.log(`${esperancita.age} es lo mismo que ${esperancita["age"]}`)
+
 // 7. Crea un objeto anidado
 
+let denver = {
+  name: "denver",
+  age: 5,
+  noviaDeDenver: {
+    name: "denvera",
+    age: 3,
+    novio: {
+      name: "denversito",
+      age: 5
+    }
+  }
+}
+
+let denver1 = {
+  name: "denver",
+  age: 5,
+  noviaDeDenver: {
+    name: "denvera",
+    age: 3,
+    novio: {
+      name: "denversito",
+      age: 5
+    }
+  }
+}
+console.log(denver)
+
 // 8. Accede y muestra el valor de las propiedades anidadas
+console.log(`${denver.name} es el nombre del perro y ${denver.noviaDeDenver.novio.age} es la edad del novio de la novia de ${denver.name}`)
+
 
 // 9. Comprueba si los dos objetos creados son iguales
+console.log(denver == denver1)
+
+
 
 // 10. Comprueba si dos propiedades diferentes son iguales
+console.log(denver.name == denver1.name)
+console.log(denver.name === denver1.name)
--- Basic/21-destructuring-spreading-exercises.js
+++ Basic/21-destructuring-spreading-exercises.js
@@ -4,22 +4,118 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=16802
 */
 
 // 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+let numeros = [1, 2, 3, 4, 5]
+
+let [uno, dos] = numeros
+
+console.log(uno)
+console.log(dos)
 
 // 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
 
+let nombres = ["Esperanza", "Salva", "Denver", "Esperancita"]
+
+let [noviaSalva, NovioEspe, HijoEspe, HijaSalva, Cristobita = "Hermano de Espe"] = nombres
+
+console.log(noviaSalva)
+console.log(NovioEspe)
+console.log(HijaSalva)
+console.log(HijoEspe)
+console.log(Cristobita)
+
 // 3. Usa desestructuración para extraer dos propiedades de un objeto
+
+let Esperancita = {
+  name: "Espe",
+  age: 30,
+  eyeColour: "green",
+}
+let { name, age } = Esperancita
+
+console.log(name)
+console.log(age)
+
 
 // 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
 //    a nuevas variables con nombres diferentes
 
+let Cristobital = {
+  name: "Cristobal",
+  age: 19,
+  eyeColour: "brown"
+}
+
+let { name: nombre, eyeColour: colorOjos } = Cristobital
+
+console.log(nombre)
+console.log(colorOjos)
+
+
+
 // 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+
+let ropero = {
+  name: "Espe",
+  age: 30,
+  eyeColour: "green",
+  espe: {
+    name: "esmeralda",
+    age: 33,
+    eyeColour: "blue"
+  }
+
+}
+
+let { name: nombre1, espe: { eyeColour: colordeOjitos } } = ropero
+console.log(nombre1)
+console.log(colordeOjitos)
 
 // 6. Usa propagación para combinar dos arrays en uno nuevo
 
+let array1 = [1, 2, 3, 4, 5]
+let array2 = ["Esperanza", "Salva"]
+
+let array3 = [...array1, ...array2]
+console.log(array3)
+console.log(array2)
+
 // 7. Usa propagación para crear una copia de un array
+
+let array4 = array2
+array4 = [...array2]
+console.log(array4)
 
 // 8. Usa propagación para combinar dos objetos en uno nuevo
 
+let AndresTomas = {
+  name: "andr3s0te",
+  age: 22,
+  height: 1.90
+}
+
+let CristobalMartin = {
+  nombre: "Cristobita",
+  edad: 19,
+  altura: 1.91
+}
+
+let mezclaHermanos = { ...AndresTomas, ...CristobalMartin }
+console.log(mezclaHermanos)
+
 // 9. Usa propagación para crear una copia de un objeto
 
+let mezcla2 = {...mezclaHermanos}
+console.log(mezcla2)
+
+const mezcla3 = mezcla2
+console.log(mezcla3)
+
 // 10. Combina desestructuración y propagación
+
+let random = [1,"Antonio",4,5,"Salvorio"]
+let random1 = [5,6,7,8,9]
+
+let [random3,random5,random6] = [...random1]
+console.log(random3)
+console.log(random5)
+console.log(random6)
