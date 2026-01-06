553981059bb5ca4c73272a65b1dc01e1ca2867a3
Salva <string.salva@gmail.com>
2026-01-06 15:47:32 +0100

    actualización hasta manejo de errores

--- Basic/25-error-handling-exercises.js
+++ Basic/25-error-handling-exercises.js
@@ -4,7 +4,7 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=20392
 */
 
 // 1. Captura una excepción utilizando try-catch
-
+/*
 let a = "salvorio"
 let b = 6
 
@@ -87,16 +87,101 @@ function multiplicarPares(a, b) {
   return a * b
 }
 
-let numeroA = 1
+let numeroA = 2
 let numeroB = 4
 
-console.log(multiplicarPares(numeroA,numeroB))
+console.log(multiplicarPares(numeroA, numeroB))
 // 6. Lanza varias excepciones según una lógica definida
+let ejemplo = 4
+let ejemplo1 = 4
+
+if (ejemplo % 2 != 0 || ejemplo1 % 2 != 0) {
+  throw new ResultError("Lo mismo de antes, o acaso eres tonto?")
+}
+
+if (ejemplo == 3) {
+  throw new ResultError("Error 404")
+}
+
+
+
+
 
 // 7. Captura varias excepciones en un mismo try-catch
 
+let experimento = 5
+let alternativa = 6
+function randomYo(experimento, alternativa) {
+  if (experimento % 2 != 0 || alternativa % 2 != 0) {
+    throw new ResultError("Lo mismo de antes, o acaso eres tonto?")
+  }
+
+  if (experimento == alternativa) {
+    throw new Error("Error 404")
+  }
+
+  console.log(experimento + alternativa)
+
+}
+
+try {
+  console.log(randomYo(experimento, alternativa))
+} catch (ResultError) {
+  if (experimento % 2 != 0) {
+    experimento += 1
+  }
+  if (alternativa % 2 != 0) {
+    alternativa += 1
+  }
+  console.log(experimento + alternativa)
+}
+
 // 8. Crea un bucle que intente transformar a float cada valor y capture y muestre los errores
 
+
+const valores = [3, 3.5, "Salvorio", "Antonio de los palotes", Math.PI]
+
+for (let i = 0; i < valores.length; i++) {
+  if (typeof valores[i] != "number") {
+    throw new TypeError
+  }
+  try {
+    console.log(` El valor ${valores[i]} es numérico y se puede pasar a Float `)
+  } catch(TypeError) {
+    console.log(` El valor ${valores[i]} no es numérico `)
+  }
+  console.log(valores[i])
+}
+
+
+*/
+
 // 9. Crea una función que verifique si un objeto tiene una propiedad específica y lance una excepción personalizada
+
+class ResultError extends Error {
+  constructor(messaje) {
+    super(messaje)
+
+  }
+}
+
+
+
+function validarObjeto(objeto, propiedad) {
+  if (Object.hasOwn(objeto, propiedad)) {
+    return true
+  }
+  return false
+}
+
+let Salva = {
+  name: "Salvorio",
+  edad: 33,
+  altura: 1.84
+}
+
+if (validarObjeto(Salva, "peso") == false) {
+  throw new ResultError("Antonio de los palotes")
+}
 
 // 10. Crea una función que realice reintentos en caso de error hasta un máximo de 10
